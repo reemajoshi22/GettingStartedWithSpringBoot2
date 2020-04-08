1f1b2473c0e769a81552ac0daa3dc11d5f7407ef
mittalp <mittalp@autodatacorp.org>
2020-04-08 12:32:05 +0530

    initial commit.

---
+++ src/main/java/com/spring/boot/learning/SpringMainClassApplication.java
@@ -0,0 +1,11 @@
+package com.spring.boot.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainClassApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMainClassApplication.class, args);
+    }
+}
